2f4f5cc7ee2f88b932d3ff3b336554a4f5d0c751
Abhijeet <abhijeet@twigsoftwares.com>
2017-01-17 13:26:18 +0530

    Drop Successfull Commit

--- app/src/main/java/com/abhijeet/myapplication/MainActivity.java
+++ app/src/main/java/com/abhijeet/myapplication/MainActivity.java
@@ -1,110 +1,82 @@
 package com.abhijeet.myapplication;import android.app.Activity;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
-import android.util.Log;
-
+import android.app.Activity;
+import android.content.ClipData;
 import android.view.DragEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    ImageView img;
-    String msg;
-    private android.widget.RelativeLayout.LayoutParams layoutParams;
+
+    Button drag;
+    LinearLayout drop;
+    TextView text,sucess;
+    int total , failure = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img=(ImageView)findViewById(R.id.imageView);
+        drag = (Button)findViewById(R.id.one);
+        drop = (LinearLayout)findViewById(R.id.bottomlinear);
+        text = (TextView)findViewById(R.id.Total);
+        sucess = (TextView)findViewById(R.id.Sucess);
 
-        img.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                ClipData.Item item = new ClipData.Item((CharSequence)v.getTag());
-                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        drop.setOnDragListener(new View.OnDragListener() {
 
-                ClipData dragData = new ClipData(v.getTag().toString(),mimeTypes, item);
-                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(img);
-
-                v.startDrag(dragData,myShadow,null,0);
-                return true;
-            }
-        });
-
-        img.setOnDragListener(new View.OnDragListener() {
             @Override
             public boolean onDrag(View v, DragEvent event) {
-                switch(event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        layoutParams = (RelativeLayout.LayoutParams)v.getLayoutParams();
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_STARTED");
+                // TODO Auto-generated method stub
+                final int action = event.getAction();
+                switch(action) {
 
-                        // Do nothing
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        break;
+
+                    case DragEvent.ACTION_DRAG_EXITED:
                         break;
 
                     case DragEvent.ACTION_DRAG_ENTERED:
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENTERED");
-                        int x_cord = (int) event.getX();
-                        int y_cord = (int) event.getY();
                         break;
 
-                    case DragEvent.ACTION_DRAG_EXITED :
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_EXITED");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        layoutParams.leftMargin = x_cord;
-                        layoutParams.topMargin = y_cord;
-                        v.setLayoutParams(layoutParams);
+                    case DragEvent.ACTION_DROP:{
+                        failure = failure+1;
+                        return(true);
+                    }
+
+                    case DragEvent.ACTION_DRAG_ENDED:{
+                        total = total +1;
+                        int suc = total - failure;
+                        sucess.setText("Sucessful Drops :"+suc);
+                        text.setText("Total Drops: "+total);
+                        return(true);
+
+                    }
+
+                    default:
                         break;
-
-                    case DragEvent.ACTION_DRAG_LOCATION  :
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_LOCATION");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        break;
-
-                    case DragEvent.ACTION_DRAG_ENDED   :
-                        Log.d(msg, "Action is DragEvent.ACTION_DRAG_ENDED");
-
-                        // Do nothing
-                        break;
-
-                    case DragEvent.ACTION_DROP:
-                        Log.d(msg, "ACTION_DROP event");
-
-                        // Do nothing
-                        break;
-                    default: break;
                 }
                 return true;
-            }
-        });
+            }});
+        drag.setOnTouchListener(new OnTouchListener() {
 
-        img.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    ClipData data = ClipData.newPlainText("", "");
-                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(img);
-
-                    img.startDrag(data, shadowBuilder, img, 0);
-                    img.setVisibility(View.INVISIBLE);
-                    return true;
-                } else {
-                    return false;
-                }
+            public boolean onTouch(View v, MotionEvent arg1) {
+                // TODO Auto-generated method stub
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadow = new View.DragShadowBuilder(drag);
+                v.startDrag(data, shadow, null, 0);
+                return false;
             }
         });
+
     }
+
 }
